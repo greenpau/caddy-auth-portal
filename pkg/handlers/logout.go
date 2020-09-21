742ee17beb45de96a2b38d24eb9948ac91e4bc0e
Paul Greenberg <greenpau@outlook.com>
2020-09-20 20:57:45 -0400

    add separation between 3 mfa types

--- pkg/handlers/logout.go
+++ pkg/handlers/logout.go
@@ -16,8 +16,13 @@ func ServeSessionLogoff(w http.ResponseWriter, r *http.Request, opts map[string]
 		zap.String("request_id", reqID),
 	)
 
-	for _, k := range cookieNames {
-		w.Header().Add("Set-Cookie", k+"=delete; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT")
+	for _, cookie := range r.Cookies() {
+		for _, cookieName := range cookieNames {
+			if cookie.Name != cookieName {
+				continue
+			}
+			w.Header().Add("Set-Cookie", cookieName+"=delete; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT")
+		}
 	}
 	w.Header().Set("Location", authURLPath)
 	w.WriteHeader(303)
