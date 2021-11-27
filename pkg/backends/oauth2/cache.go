e962711abd5981fcdf65a4d6dbe55b51d00c4472
Jayson Reis <santosdosreis@gmail.com>
2021-11-27 09:28:00 +0100

    chore: Remove unused code

--- pkg/backends/oauth2/cache.go
+++ pkg/backends/oauth2/cache.go
@@ -11,26 +11,3 @@ func getStateCacheKey(state string) string {
 func getCodeCacheKey(state string) string {
 	return fmt.Sprintf("caddy-auth-portal-code-%s", state)
 }
-
-// ValidateNonce validate a nonce for the give state.
-func (b *Backend) validateNonce(state, nonce string) error {
-	stateKey := getStateCacheKey(state)
-	stateExists, err := b.cache.Exists(stateKey)
-	if err != nil {
-		return fmt.Errorf("failed to check if state exists %v", err)
-	}
-	if !stateExists {
-		return fmt.Errorf("no once found for %s", stateKey)
-	}
-
-	var storedState string
-
-	err = b.cache.Get(stateKey, &storedState)
-	if err != nil {
-		return fmt.Errorf("failed to fetch state %s %w", state, err)
-	}
-	if storedState != nonce {
-		return fmt.Errorf("nonce mismatch %s (expected) vs. %s (received)", storedState, nonce)
-	}
-	return nil
-}
